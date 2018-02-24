4a95a6a7c3e973a726ff04414ded54cd6d046602
DevAlone <admin@d3d.info>
2018-02-24 21:52:06 +0300

    something

--- restycorn/server.py
+++ restycorn/server.py
@@ -15,17 +15,49 @@ class Server:
         self.base_address = '/'
         self.app = web.Application()
         self.access_log_format = access_log_format
+        self.pre_request_function = None
+        self.default_handler = None
 
     def run(self):
+        if self.default_handler is not None:
+            self.app.router.add_route(
+                'GET',
+                '/{tail:.*}',
+                lambda *args, **kwargs: self.request_handler(handler=self.default_handler, *args, **kwargs)
+            )
+
         web.run_app(self.app, host=self.host, port=self.port, access_log_format=self.access_log_format)
 
     def register_resource(self, resource_name, resource: BaseResource):
         handler = ResourceRequestHandler(resource)
         resource_url = self.base_address + '/' + resource_name
 
-        self.app.router.add_route('*', resource_url, handler.request_resource)
-        self.app.router.add_route('*', resource_url + '/', handler.request_resource)
-        self.app.router.add_route('*', resource_url + '/{id}', handler.request_resource_item)
+        self.app.router.add_route(
+            '*',
+            resource_url,
+            lambda *args, **kwargs: self.request_handler(handler=handler.request_resource, *args, **kwargs)
+        )
+        self.app.router.add_route(
+            '*',
+            resource_url + '/',
+            lambda *args, **kwargs: self.request_handler(handler=handler.request_resource, *args, **kwargs)
+        )
+        self.app.router.add_route(
+            '*',
+            resource_url + '/{id}',
+            lambda *args, **kwargs: self.request_handler(handler=handler.request_resource_item, *args, **kwargs)
+        )
 
     def set_base_address(self, base_address: str):
         self.base_address = base_address
+
+    async def request_handler(self, request, handler):
+        if self.pre_request_function:
+            pre_result = await self.pre_request_function(request)
+            if pre_result is not None:
+                return pre_result
+
+        return await handler(request)
+
+    def set_default_route(self, handler):
+        self.default_handler = handler
